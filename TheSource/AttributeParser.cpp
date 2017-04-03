fd203e717a62a42c04b7b717ecc54c41b5aed592
epombo <epombo@goodgamestudios.com>
2017-04-03 18:13:51 +0200

    [TS] Parser working against first TestCase

--- TheSource/AttributeParser.cpp
+++ TheSource/AttributeParser.cpp
@@ -5,21 +5,16 @@
 #include <map>
 #include <algorithm> // std::count
 
+//#define INT_MAX       2147483647    // maximum (signed) int value
+
 class Tag
 {
-private:
-
-	std::string TagName;
-	std::string TagContents;
-	std::vector<Tag*> ChildrenTags;
-	std::map<std::string, std::string> TagAttributes;
-
 	void ParseAttributes()
 	{
 		// Using the TagContents, parse all attributes
 		std::size_t lastFoundPos = 0;
 		
-		std::size_t AssignmentCount = std::count(TagContents.begin(), TagContents.end(), '=');
+		int AssignmentCount = std::count(TagContents.begin(), TagContents.end(), '=');
 		for (int i = 0; i < AssignmentCount; ++i)
 		{
 			// Find the next "=" sign
@@ -31,8 +26,8 @@ private:
 			// Copy contents from right side
 			auto attrValue = SubstrValueFromContent(lastFoundPos, false);
 
-			std::cout << "attrName found!: " << attrName << std::endl;
-			std::cout << "attrValue found!: " << attrValue << std::endl;
+			//std::cout << "attrName found!: " << attrName << std::endl;
+			//std::cout << "attrValue found!: " << attrValue << std::endl;
 
 			// Add to map
 			TagAttributes.insert(std::make_pair(attrName, attrValue));
@@ -59,7 +54,7 @@ private:
 				}
 				else if (_char == ' ' && substringEnd != 0 && substringStart == 0)
 				{
-					substringStart = somePos;
+					substringStart = somePos + 1; // +1: Do not count empty space
 					bEndReached = true;
 				}
 			}
@@ -89,6 +84,13 @@ private:
 
 public:
 
+	std::string TagName;
+	std::string TagContents;
+	std::vector<Tag> ChildrenTags;
+	std::map<std::string, std::string> TagAttributes;
+
+	Tag(){}
+
 	Tag(std::string _TagName, std::string _TagContents)
 	{
 		TagName = _TagName;
@@ -99,22 +101,33 @@ public:
 	bool HasChildren() const { return ChildrenTags.size() > 0; }
 
 	// Adds a new tag to our children
-	void AddChildTag(Tag* _NewChild)
+	void AddChildTag(Tag _NewChild)
 	{
 		ChildrenTags.push_back(_NewChild);
 	}
 
 	// Find any child tags given a tag name
-	Tag* FindChildTagByName(std::string _TagName)
+	Tag FindChildTagByName(std::string _TagName)
 	{
 		for (auto TagRef : ChildrenTags)
 		{
-			if (TagRef->TagName == _TagName)
+			if (TagRef.TagName == _TagName)
 			{
 				return TagRef;
 			}
 		}
-		return nullptr;
+	}
+
+	std::string FindAttrValueByAttrName(std::string _AttrName)
+	{
+		for (auto TagAttr : TagAttributes)
+		{
+			if (TagAttr.first == _AttrName)
+			{
+				return TagAttr.second;
+			}
+		}
+		return "";
 	}
 };
 
@@ -141,7 +154,19 @@ public:
 	HelloWorld
 */
 
-std::string AttributeParser::FindTagName(std::string _TagLine, std::size_t _FoundTagPos)
+Tag AttributeParser::FindTagByName(std::vector<Tag> _HRMLTags, std::string _TagName)
+{
+	for (auto TagRef : _HRMLTags)
+	{
+		if (TagRef.TagName == _TagName)
+		{
+			return TagRef;
+		}
+	}
+	return Tag();
+}
+
+std::string AttributeParser::FindTagNameFromTagLine(std::string _TagLine, std::size_t _FoundTagPos)
 {
 	// Go to _FoundTagPos and copy each char until you have a space or eol
 	std::string TagName = "";
@@ -168,10 +193,10 @@ AttributeParser::AttributeParser()
 
 void AttributeParser::Execute()
 {
-	std::cout << "Please insert the number of lines and number of queries. e.g. 2 1" << std::endl;
+	//std::cout << "Please insert the number of lines and number of queries. e.g. 2 1" << std::endl;
 
-	std::vector<Tag*> HRMLTags;
-	std::vector<int> Queries;
+	std::vector<Tag> HRMLTags;
+	std::vector<std::string> Queries;
 	int NumberOfLines;
 	int QueryAmount;
 
@@ -189,9 +214,8 @@ void AttributeParser::Execute()
 	std::cin.ignore(INT_MAX, '\n');
 
 	bool TagOpened = false;
-	Tag* StoredParentTag = nullptr;
 
-	// Read line
+	// Read lines "Tags"
 	for (int i = 0; i < NumberOfLines; ++i)
 	{
 		std::cout << "Please insert line number " << i+1 << std::endl;
@@ -206,26 +230,28 @@ void AttributeParser::Execute()
 		if (foundSlash == std::string::npos && foundTag != std::string::npos)
 		{
 			// Found a tag
-			std::cout << "Line is a tag" << std::endl;
+			//std::cout << "Line is a tag" << std::endl;
 
 			if (TagOpened)
 			{
 				// This is a child Tag, get a reference to the parent tag and add it to the list
-				std::string TagName = FindTagName(line, foundTag);
+				std::string TagName = FindTagNameFromTagLine(line, foundTag);
 				Tag NewTag(TagName, line);
-				StoredParentTag->AddChildTag(&NewTag);
+				
+				// TODO: find which tag has closed to add tag to the proper parent
+				// Add tag to the last tag "parent" added
+				HRMLTags.back().AddChildTag(NewTag);
 			}
 			else
 			{
 				TagOpened = true;
 
 				// Find tag name using the starting pos on TagOpened
-				std::string TagName = FindTagName(line, foundTag);
+				std::string TagName = FindTagNameFromTagLine(line, foundTag);
 
 				// Create a new Tag
 				Tag NewTag(TagName, line);
-				HRMLTags.push_back(&NewTag);
-				StoredParentTag = (&NewTag);
+				HRMLTags.push_back(NewTag);
 			}
 
 		}
@@ -236,19 +262,92 @@ void AttributeParser::Execute()
 		}
 		else
 		{
+			// TODO: find which tag has closed to add tag to the proper parent
 			// Line was an ending tag
-			std::cout << "line is an ending tag :: Line: " << line << std::endl;
+			//std::cout << "line is an ending tag :: Line: " << line << std::endl;
 			TagOpened = false;
 		}
 	}
 
-	// TODO: Process queries on the saved Tags
-	/*for (int i = 0; i < QueryAmount; ++i)
+	// Store queries
+	for (int i = 0; i < QueryAmount; ++i)
 	{
+		// Store query
+		std::string query;
+		std::getline(std::cin, query);
 
-	}*/
+		Queries.push_back(query);
+	}
 
-	
+	// Finally process queries and output values!
+	for (int i = 0; i < static_cast<int>(Queries.size()); ++i)
+	{
+		auto query = Queries[i];
+
+		// query example: tag1.tag2~name
+
+		// find tag
+		std::size_t foundTilde = query.find("~");
+
+		int DotCount = std::count(query.begin(), query.end(), '.');
+		if (DotCount > 0)
+		{
+			// found a dot, take parent name and try to find the child being called
+			std::size_t foundDot = query.find(".");
+			auto ParentTagName = query.substr(0, foundDot);
+			auto TagQueried = FindTagByName(HRMLTags, ParentTagName);
+
+			for (int i = 0; i < DotCount; ++i)
+			{
+				if (DotCount > 1)
+				{
+					auto firstDot = query.find_first_of(".", foundDot + 1); // Find next dot after the one we found on the parent
+					auto subTagName = query.substr(foundDot, firstDot);
+
+					if (TagQueried.HasChildren())
+					{
+						TagQueried = TagQueried.FindChildTagByName(subTagName);
+					}
+					else
+					{
+						break;
+					}
+				}
+				else
+				{
+					auto subTagName = query.substr(foundDot + 1, foundDot);
+
+					if (TagQueried.HasChildren())
+					{
+						TagQueried = TagQueried.FindChildTagByName(subTagName);
+					}
+					else
+					{
+						break;
+					}
+				}
+			}
+
+			// find attr
+			auto attrToRead = query.substr(foundTilde + 1, query.size());
+			auto attrValue = TagQueried.FindAttrValueByAttrName(attrToRead);
+
+			// output query
+			auto output = attrValue != "" ? attrValue : "Not Found!";
+			std::cout << output << std::endl;
+		}
+		else if (foundTilde != std::string::npos)
+		{
+			auto tagName = query.substr(0, foundTilde);
+			auto TagQueried = FindTagByName(HRMLTags, tagName);
+			auto attrToRead = query.substr(foundTilde + 1, query.size());
+			auto attrValue = TagQueried.FindAttrValueByAttrName(attrToRead);
+
+			// output query
+			auto output = attrValue != "" ? attrValue : "Not Found!";
+			std::cout << output << std::endl;
+		}
+	}
 }
 
 
--- TheSource/AttributeParser.h
+++ TheSource/AttributeParser.h
@@ -1,14 +1,18 @@
 #pragma once
 #include <string>
+#include <vector>
 
 /// hackerrank: https://www.hackerrank.com/challenges/attribute-parser
+
+class Tag;
 
 class AttributeParser
 {
 
 private:
 
-	std::string FindTagName(std::string _TagLine, std::size_t _FoundTagPos);
+	Tag FindTagByName(std::vector<Tag> _HRMLTags, std::string _TagName);
+	std::string FindTagNameFromTagLine(std::string _TagLine, std::size_t _FoundTagPos);
 
 public:
 	AttributeParser();
