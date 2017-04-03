13213a072ac930320118142da89d6b1f809b9ee3
epombo <epombo@goodgamestudios.com>
2017-04-03 15:43:43 +0200

    [TS] Parsing Tag attributes into a map

--- TheSource/AttributeParser.cpp
+++ TheSource/AttributeParser.cpp
@@ -2,12 +2,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
-class TagAttributes
-{
-public:
-	TagAttributes(){}
-};
+#include <map>
+#include <algorithm> // std::count
 
 class Tag
 {
@@ -16,7 +12,80 @@ private:
 	std::string TagName;
 	std::string TagContents;
 	std::vector<Tag*> ChildrenTags;
-	std::vector<TagAttributes> TagAttributes;
+	std::map<std::string, std::string> TagAttributes;
+
+	void ParseAttributes()
+	{
+		// Using the TagContents, parse all attributes
+		std::size_t lastFoundPos = 0;
+		
+		std::size_t AssignmentCount = std::count(TagContents.begin(), TagContents.end(), '=');
+		for (int i = 0; i < AssignmentCount; ++i)
+		{
+			// Find the next "=" sign
+			lastFoundPos = TagContents.find_first_of("=", lastFoundPos + 1);
+
+			// Copy contents from left side
+			auto attrName = SubstrValueFromContent(lastFoundPos, true);
+
+			// Copy contents from right side
+			auto attrValue = SubstrValueFromContent(lastFoundPos, false);
+
+			std::cout << "attrName found!: " << attrName << std::endl;
+			std::cout << "attrValue found!: " << attrValue << std::endl;
+
+			// Add to map
+			TagAttributes.insert(std::make_pair(attrName, attrValue));
+		}
+	}
+
+	std::string SubstrValueFromContent(std::size_t assignmentPos, bool bSubstrToTheLeft)
+	{
+		bool bEndReached = false;
+		auto somePos = assignmentPos;
+		std::size_t substringStart = 0;
+		std::size_t substringEnd = 0;
+		std::string AttrValue = "";
+
+		// Copy contents from left side
+		if (bSubstrToTheLeft)
+		{
+			while (!bEndReached)
+			{
+				auto _char = TagContents[--somePos];
+				if (_char != ' ' && substringEnd == 0)
+				{
+					substringEnd = somePos;
+				}
+				else if (_char == ' ' && substringEnd != 0 && substringStart == 0)
+				{
+					substringStart = somePos;
+					bEndReached = true;
+				}
+			}
+			auto AmountToSubstr = (substringEnd - substringStart) + 1;
+			AttrValue = TagContents.substr(substringStart, AmountToSubstr);
+		}
+		else
+		{
+			while (!bEndReached)
+			{
+				auto _char = TagContents[++somePos];
+				if (_char == '"' && substringStart == 0)
+				{
+					substringStart = somePos + 1; // +1: Do not count the quotes
+				}
+				else if (_char == '"' && substringEnd == 0 && substringStart != 0)
+				{
+					substringEnd = somePos - 1; // -1: Do not count the quotes
+					bEndReached = true;
+				}
+			}
+			auto AmountToSubstr = (substringEnd - substringStart) + 1;
+			AttrValue = TagContents.substr(substringStart, AmountToSubstr);
+		}
+		return AttrValue;
+	}
 
 public:
 
@@ -24,6 +93,7 @@ public:
 	{
 		TagName = _TagName;
 		TagContents = _TagContents;
+		ParseAttributes();
 	}
 
 	bool HasChildren() const { return ChildrenTags.size() > 0; }
@@ -173,10 +243,10 @@ void AttributeParser::Execute()
 	}
 
 	// TODO: Process queries on the saved Tags
-	for (int i = 0; i < QueryAmount; ++i)
+	/*for (int i = 0; i < QueryAmount; ++i)
 	{
 
-	}
+	}*/
 
 	
 }
